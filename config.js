a825a62518e009ca0783d87c802a79a9d8d366dd
BlackNeo <webscribo@gmail.com>
2019-06-02 16:33:27 +0200

    1.0.0: iTunes test version beta - PhoneGap Build cli-6.5.0

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'bbs-itunes',
+		wp_ws_url : 'https://bibinesports.fr/wp-appkit-api/bbs-itunes',
+		wp_url : 'https://bibinesports.fr',
+		theme : 'q-ios',
+		version : '1.0.0',
+		app_type : 'phonegap-build',
+		app_title : 'Bibine Sports',
+		app_platform : 'ios',
+		app_path: '',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'lBx3+sbL7X7-kL@!T=[we!bz(Lw_CP |L!H?bL?,wSWx(,eNz.)~k*VuY1@IMKhK',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
